16ac3ef34378aed50ec4964d318a6f46e3d151c5
sofia-morleo <sofymleo@gmail.com>
2024-08-29 20:40:07 -0300

    Clase 6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo ¿Como estan?");
+// console.log("Hola Mundo ¿Como estan?");
+const  h5 = document.querySelector("h5");
+const text =  document.querySelector(".card-brand")
+const  precio = document.querySelector(".card-price");
+const img = document.querySelector(".card-img-top");
+
+h5.innerText = "Dibu"
+text.innerText = "Futbolista argentino";
+precio.innerText = "$7,29M"
+img.src = "https://img.lagaceta.com.ar/fotos/notas/2023/12/27/600x400_para-nosotros-el-mejor-emiliano-dibu-martinez-termino-detras-brasileno-ederson-manchester-city-1018785-110841.webp"; 
