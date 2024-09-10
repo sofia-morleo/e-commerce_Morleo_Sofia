58cd8452b9f7759381c9e422a1fe348bd66c740f
sofia-morleo <sofymleo@gmail.com>
2024-09-10 20:39:19 -0300

    spring 1

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// console.log("Hola Mundo ¿Como estan?");
-// const  h5 = document.querySelector("h5");
-// const text =  document.querySelector(".card-brand")
-// const  precio = document.querySelector(".card-price");
-// const img = document.querySelector(".card-img-top");
-
-// h5.innerText = "Dibu"
-// console.log(h5.innerText)
-// text.innerText = "Futbolista argentino";
-// console.log(text.innerText)
-// precio.innerText = "$7,29M"
-// console.log(precio.innerText)
-// img.src = "https://img.lagaceta.com.ar/fotos/notas/2023/12/27/600x400_para-nosotros-el-mejor-emiliano-dibu-martinez-termino-detras-brasileno-ederson-manchester-city-1018785-110841.webp"; 
-// console.log(img.src)
-
 const titulo = document.querySelector("h1");
-titulo.innerText = "Productos"
+titulo.innerText = "Productos";
 
-const Prod = []
-for (let i = 1; i <= 5; i++) {
+const prod = [];
+for (let i = 1; i < 10; i++) {
 
-    const Card = `
-        <div class="card col-sm-12 col-md-6 col-lg-4 mb-4">
-            <a href="#"><img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="..."></a>
+    const card = `
+        <div class="card mb-3" style="width: 18rem;">
+            <a href="#"><img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;" alt="..."></a>
             <div class="card-body">
                     <h5 class="card-title">Producto ${i}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -29,8 +14,10 @@ for (let i = 1; i <= 5; i++) {
             </div>
         </div>`
 
-        Prod.push(Card);
+    prod.push(card);
+
 }
 
-// document.querySelector('section').innerHTML = (Prod); //esto estaba bien pero cuando lo mostraba habia comas
-document.querySelector('section').innerHTML = Prod.join("");
+document.querySelector('section').innerHTML = prod.join().replaceAll(",", "");
+
+
