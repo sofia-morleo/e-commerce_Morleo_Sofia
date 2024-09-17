df253368c284aae78bcfaceda8648a740f717b07
sofia-morleo <sofymleo@gmail.com>
2024-09-17 17:41:11 -0300

    clase 10

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const titulo = document.querySelector("h1");
-titulo.innerText = "Productos";
+function card() {
+    const titulo = document.querySelector("h1");
+    titulo.innerText = "Productos";
 
-const prod = [];
-for (let i = 1; i < 10; i++) {
+    const prod = [];
+    for (let i = 1; i < 10; i++) {
 
-    const card = `
+        const card = `
         <div class="card mb-3" style="width: 18rem;">
             <a href="#"><img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;" alt="..."></a>
             <div class="card-body">
                     <h5 class="card-title">Producto ${i}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary bg-secondary border-0">Agregar</a>
+                    <a href="#" class="btn btn-primary bg-secondary border-0">Ver más</a>
             </div>
         </div>`
 
-    prod.push(card);
+        prod.push(card);
 
+    }
+
+    document.querySelector('section').innerHTML = prod.join().replaceAll(",", "");
 }
-
-document.querySelector('section').innerHTML = prod.join().replaceAll(",", "");
+card();
 
 
